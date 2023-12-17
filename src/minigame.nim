a0c8e5e87bdd892dba264dd54ae86cbc681e9b7d
TLP <spektralfrogadier@gmail.com>
2023-12-17 11:53:05 -0800

    Useability Update
    About 45% complete...

---
+++ src/minigame.nim
@@ -0,0 +1,9 @@
+#[ Atom Creator Mini-Game:
+This application provides a togglable minigame where you can create your atom \
+then try to match it to an existing element from the periodic table.
+]#
+
+import atom_creator
+
+proc runMain*() =
+  echo("I am the minigame!")
